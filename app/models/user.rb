01d44c7f8cebe754b11745df3293579e71559dce
Nima Azarbayjani <nimaza@gmail.com>
2020-11-09 16:10:55 +0330

    added the booking model and set the validation and associations of all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :email, :password, :first_name, :last_name, presence: true
+
+  has_many :bookings, dependent: :destroy
+  belongs_to :place, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
