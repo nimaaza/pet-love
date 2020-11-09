ab6f431f8fe17b4b1d3e123fdcf83eec436add5e
Nima Azarbayjani <nimaza@gmail.com>
2020-11-09 19:27:20 +0330

    added validations for title and price to place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ApplicationRecord
-  validates :full_address, :capacity, :description, :photo_url, presence: true
+  validates :full_address, :capacity, :description, presence: true
+  validates :photo_url, :title, :price, presence: true
   validates :capacity, inclusion: { in: (1..10) }
+  validates :price, inclusion: { in: (0..1000) }
 
   belongs_to :user
   has_many :bookings
