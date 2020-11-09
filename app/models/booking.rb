941033c1a10ad143649c0a368de6f08271cf1006
Nima Azarbayjani <nimaza@gmail.com>
2020-11-09 21:16:59 +0330

    removed the validation for boolean value confirmed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   validates :booking_start_date, :booking_end_date, presence: true
-  validates :confirmed, :pet_description, :species, presence: true
+  validates :pet_description, :species, presence: true
 
   belongs_to :place
   belongs_to :user
