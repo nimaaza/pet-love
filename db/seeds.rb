fea471131e3ab51182fb308df517081a02e8edfe
Nima Azarbayjani <nimaza@gmail.com>
2020-11-09 15:52:31 +0330

    create 10 fake places in the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.destroy_all
+
+10.times do
+  Place.create(
+    full_address: Faker::Address.full_address,
+    description: Faker::Lorem.sentence,
+    photo_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/greece.jpg',
+    capacity: rand(1..5),
+    user_id: 1,
+  )
+end
