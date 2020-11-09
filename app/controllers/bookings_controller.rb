cdbae22316155731e6d228c13b27ebcd4a36f7c6
Nima Azarbayjani <nimaza@gmail.com>
2020-11-09 18:46:41 +0330

    added the confirm actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,10 @@ class BookingsController < ApplicationController
   def my_requests
     @my_requests = Booking.where(user: current_user)
   end
+
+  def confirm
+    @booking = Booking.find(params[:id])
+    @booking.confirmed = true
+    @booking.save
+  end
 end
