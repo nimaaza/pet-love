2f4902311bebed7bfd63d461c1eba5f2f044847b
Nima Azarbayjani <nimaza@gmail.com>
2020-11-12 13:58:24 +0330

    change to cloudinary service for photos

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
   validates :full_address, :capacity, :description, presence: true
-  validates :photo_url, :title, :price, presence: true
+  validates :title, :price, presence: true
   validates :capacity, inclusion: { in: (1..10) }
   validates :price, inclusion: { in: (0..1000) }
   validates :user, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, :password, :first_name, :last_name, presence: true
+  validates :email, :password, presence: true
 
   has_many :bookings
   has_one :place
