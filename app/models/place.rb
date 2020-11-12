7cbde85dd33dab065c0c1f9b2d5d37c27b49a604
Nima Azarbayjani <nimaza@gmail.com>
2020-11-12 03:32:20 +0330

    search backend work using pg_search

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,8 +10,11 @@ class Place < ApplicationRecord
   has_one_attached :photo
 
   include PgSearch::Model
-  pg_search_scope :search_address,
+  pg_search_scope :search_address_host,
                   against: [:full_address, :title],
+                  associated_against: {
+                    user: [:first_name, :last_name]
+                  },
                   using: {
                     tsearch: { prefix: true }
                   }
